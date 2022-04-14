562a01fa44ab4264f24b0e39758695d848ccd094
Andrés Fresno <andfres@gmail.com>
2022-04-14 23:51:23 +0200

    avatar managers

--- src/components/Productores.vue
+++ src/components/Productores.vue
@@ -1,30 +1,23 @@
 <template>
-      <div class="productores">
-      <Productor
-        v-for="(item, i) in productores"
-        :key="i"
-        v-bind="productores[i]"
-      >
-      </Productor>
-    </div>
+  <div class="productores">
+    <Productor
+      v-for="(item, i) in productores"
+      :key="i"
+      v-bind="productores[i]"
+    >
+    </Productor>
+  </div>
 </template>
 
 <script setup>
 import Productor from "@/components/Productor.vue";
 
-import { creaProductores } from "@/utils/creaProductores";
-
 import { storeToRefs } from "pinia";
 import { useProductoresStore } from "@/store/productores";
 
-const productoresCreados = creaProductores();
 const productoresStore = useProductoresStore();
 
-productoresStore.productores = [...productoresCreados];
 const { productores } = storeToRefs(productoresStore);
-
-
-
 </script>
 
 <style>
