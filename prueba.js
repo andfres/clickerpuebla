4fa0b44c806d81e53daec9b94b3378372e946e2a
Andrés Fresno <andfres@gmail.com>
2022-05-06 12:11:16 +0200

    sdfs

--- prueba.js
+++ prueba.js
@@ -2,3 +2,11 @@
 // git push heroku master
 //heroku logs --tail
 //heroku config:set NODE_MODULES_CACHE=false
+
+
+
+// "test:unit": "vitest --environment jsdom",
+// "lint": "eslint . --ext .vue,.js,.jsx,.cjs,.mjs --fix --ignore-path .gitignore"
+
+
+console.log("ha funcionado")
