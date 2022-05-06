48a9c11988f18e0feb5c0fc75b47c1d6b88a46fa
Andrés Fresno <andfres@gmail.com>
2022-05-06 13:48:44 +0200

    a

--- prueba.js
+++ prueba.js
@@ -1,17 +1,25 @@
 // console.log("NPM_CONFIG_PRODUCTION" , process.env.NPM_CONFIG_PRODUCTION);
-// git push heroku master
 
+
+
+// git push heroku master
 //heroku logs --tail
+
 //heroku config:set NODE_MODULES_CACHE=false
+//heroku config:set NODE_ENV=production
 
 
 //https://devcenter.heroku.com/articles/dynos
 //heroku ps 
 //heroku ps:restart
 
-
+// "start_prueba": "npm run prueba",
+// "prueba" : "node prueba.js"
 // "test:unit": "vitest --environment jsdom",
 // "lint": "eslint . --ext .vue,.js,.jsx,.cjs,.mjs --fix --ignore-path .gitignore"
+
+
+//heroku buildpacks:set heroku/nodejs -a clickerpueblo
 
 
 console.log("ha funcionado")
