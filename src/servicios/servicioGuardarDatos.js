1c53269a101394dfa99bb8a3454f968e751c1751
Andrés Fresno <andfres@gmail.com>
2022-06-14 15:53:29 +0200

    guardar datos

---
+++ src/servicios/servicioGuardarDatos.js
@@ -0,0 +1,36 @@
+let location = "localhost:8080/api";
+
+
+const servicioGuardarDatos = async (datos_guardar) => {
+
+  console.log("datos que se van a mandar" , datos_guardar);
+
+  
+
+  // console.log("data", data);
+  // const settings = {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-type": "application/json;charset=UTF-8",
+  //   },
+  //   body: JSON.stringify(data),
+  // };
+
+  // let text, ok;
+
+  // try {
+  //   const fetchResponse = await fetch(`http://${location}/guardar`, settings);
+  //   text = await fetchResponse.text();
+  //   ok = fetchResponse.ok;
+  // } catch (e) {
+  //   //console.log(e);
+  //   throw new Error("Error de conexión al llamar al API"); // Lanzar el error
+  // }
+
+  // if(!ok) {
+  //   throw new Error("Error de registro - " + text);
+  // }
+
+};
+
+export default servicioGuardarDatos;
